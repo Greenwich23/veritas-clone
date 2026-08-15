586e1cf37b6cf265aa4616aca53361b9cd060dbe
Greenwich23 <nosaomo83@gmail.com>
2026-08-16 00:00:43 +0100

    CREATED THE TUITION SHI

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to top on route change
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  // Also scroll to top on initial mount
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return null;
+}
